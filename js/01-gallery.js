619dcaf7432f02aabf53913d81fdd5b4952d5dde
ribacot <remontdiskov1979@gmail.com>
2023-05-25 10:01:55 +0300

    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,10 +2,16 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("afterbegin", murkup(galleryItems));
+galleryEl.addEventListener("click", hendleGalleryClick);
+document.addEventListener("keydown", HendleCloseEsc);
 
-const murkup = galleryItems
-	.map(
-		({ preview, original, description }) => `<li class="gallery__item">
+let instance = null;
+
+function murkup(gallery) {
+	return gallery
+		.map(
+			({ preview, original, description }) => `<li class="gallery__item">
   <a class="gallery__link" href='#'>
     <img
       class="gallery__image"
@@ -15,31 +21,23 @@ const murkup = galleryItems
     />
   </a>
 </li>`
-	)
-    .join("");
-    
-galleryEl.insertAdjacentHTML("afterbegin", murkup);
+		)
+		.join("");
+}
 
-let instance = null
-
-const hendleGalleryClick = (e) => {
+function hendleGalleryClick  (e) {
 	e.preventDefault();
 	const largeImgSrc = e.target.dataset.source;
-	 instance = basicLightbox.create(`
+	instance = basicLightbox.create(`
 	    <img class="largeImg" src=${largeImgSrc} width="800" height="600">
 	`);
 	instance.show();
 };
 
-const HendleCloseEsc = function (e) {
-    if (e.key === "Escape" || e.key === "Enter") {
-        e.preventDefault();
-       instance.close();
-
-		// const largeImgEl = document.querySelector(".basicLightbox--visible");
-		// largeImgEl.classList.remove("basicLightbox--visible");
+function HendleCloseEsc(e) {
+	if (e.key === "Escape" || e.key === "Enter") {
+		e.preventDefault();
+		instance.close();
 	}
-};
+}
 
-document.addEventListener("keydown", HendleCloseEsc);
-galleryEl.addEventListener("click", hendleGalleryClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,10 +2,18 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("afterbegin", murkup(galleryItems));
+const lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+	overlayOpacity: 0.9,
+	closeText: "&#10007;",
+});
 
-const murkup = galleryItems
-	.map(
-		({ preview, original, description }) => `<li class="gallery__item gallery " >
+function murkup(gallery) {
+	return gallery
+		.map(
+			({ preview, original, description }) => `<li class="gallery__item gallery " >
   <a class="gallery__link" href='${original}' >
     <img
       class="gallery__image"
@@ -15,14 +23,8 @@ const murkup = galleryItems
     />
   </a>
 </li>`
-	)
-	.join("");
+		)
+		.join("");
+}
 
-galleryEl.insertAdjacentHTML("afterbegin", murkup);
 
-const lightbox = new SimpleLightbox(".gallery a", {
-	captionsData: "alt",
-	captionDelay: 250,
-	overlayOpacity: 0.9,
-	closeText: "&#10007;",
-});
